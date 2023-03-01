84fd2a67c61decc0ce712d25139ec985a7a7335b
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-01 13:22:11 -0800

    Nav bar now highlights to show which page is selected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,33 @@
 import Resume from "../assets/Resume-JameyWicklund.pdf"
 
-export default function NavBar({currentPage, handlePageChange}) {
+export default function NavBar({ currentPage, handlePageChange }) {
   return (
     <div className="bg-emerald-700 text-center">
       <div className="grid gap-1 grid-cols-4 mx-0">
-        <a href="#aboutme" onClick={() => handlePageChange('AboutMe')} className="text-green-200">About Me</a>
-        <a href="#contact" onClick={() => handlePageChange('Contact')} className="text-green-200">Contact</a>
-        <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className="text-green-200">Portfolio</a>
-        <a href={Resume} className="text-green-200" download>Resume</a>
+        <a
+          href="#aboutme"
+          onClick={() => handlePageChange('AboutMe')}
+          className={currentPage === 'AboutMe' ? 'text-teal-300 font-semibold' : 'text-green-200'}>
+          About Me
+        </a>
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'text-teal-300 font-semibold' : 'text-green-200'}>
+          Contact
+        </a>
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'text-teal-300 font-semibold' : 'text-green-200'}>
+          Portfolio
+        </a>
+        <a
+          href={Resume}
+          className="text-green-200"
+          download>
+          Resume
+        </a>
       </div>
     </div >
   )
