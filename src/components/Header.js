1c30fae2cd7ec5e3679683a6283e9fe96362cf0c
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-01 12:36:55 -0800

    Styles Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import NavBar from "./NavBar"
 export default function Header() {
   return (
     <header>
-      <img src={profilePic} alt="Jamey Wicklund" />
+      <img src={profilePic} alt="Jamey Wicklund" className="max-h-80 rounded-full pl-6 mt-3"/>
+      <h1 className="text-right mr-5 text-2xl font-medium text-teal-500">Jamey Wicklund</h1>
       <NavBar />
     </header>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,13 @@
 
 export default function NavBar() {
   return (
-    <div>
-      <a href="#aboutme">About Me</a>
-      <a href="#contact">Contact</a>
-      <a href="#portfolio">Portfolio</a>
-      <a href="#resume">Resume</a>
-    </div>
+    <div className="bg-emerald-700 text-center">
+      <div className="grid gap-1 grid-cols-4 mx-0">
+        <a href="#aboutme" className="text-green-200">About Me</a>
+        <a href="#contact" className="text-green-200">Contact</a>
+        <a href="#portfolio" className="text-green-200">Portfolio</a>
+        <a href="#resume" className="text-green-200">Resume</a>
+      </div>
+    </div >
   )
 }
