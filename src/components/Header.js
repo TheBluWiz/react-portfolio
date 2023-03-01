eede7716c02f84d6e75659f904a4f1b0a68d659d
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-01 13:10:01 -0800

    Nav now functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import profilePic from "../assets/images/profilePic.jpeg";
 import NavBar from "./NavBar"
 
-export default function Header() {
+export default function Header({currentPage, setCurrentPage}) {
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <header>
       <img src={profilePic} alt="Jamey Wicklund" className="max-h-72 rounded-full pl-6 mt-3"/>
       <h1 className="text-right mr-5 text-2xl font-medium text-teal-500">Jamey Wicklund</h1>
-      <NavBar />
+      <NavBar currentPage={currentPage} handlePageChange={handlePageChange}/>
     </header>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 
 
-export default function NavBar() {
+export default function NavBar({currentPage, handlePageChange}) {
   return (
     <div className="bg-emerald-700 text-center">
       <div className="grid gap-1 grid-cols-4 mx-0">
-        <a href="#aboutme" className="text-green-200">About Me</a>
-        <a href="#contact" className="text-green-200">Contact</a>
-        <a href="#portfolio" className="text-green-200">Portfolio</a>
-        <a href="#resume" className="text-green-200">Resume</a>
+        <a href="#aboutme" onClick={() => handlePageChange('AboutMe')} className="text-green-200">About Me</a>
+        <a href="#contact" onClick={() => handlePageChange('Contact')} className="text-green-200">Contact</a>
+        <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className="text-green-200">Portfolio</a>
+        <a href="../assets/Resume-JameyWicklund.pdf" className="text-green-200" download>Resume</a>
       </div>
     </div >
   )
