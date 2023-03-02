b6ff6e18de47d39599a160a5114086ba8034e9e5
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-02 13:01:55 -0800

    Adds array for portfolio. Displays website images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,69 @@
-
+import devQuizPic from '../assets/images/dev-quiz.png';
+import giphtionaryPic from '../assets/images/giphtionary.png';
+import mailMeSeedsPic from '../assets/images/mail-me-seeds.png';
+import noteTaker from '../assets/images/note-taker.png';
+import teamProfileGenerator from '../assets/images/team-profile-generator.png';
+import techSpotBlot from '../assets/images/tech-spot-blot.png';
 
 
 
 export default function Contact() {
-  return (
-   <p>The Portfolio</p>
-  );
+  const cardArray = [
+    {
+      title: "Coding Quiz",
+      image: devQuizPic,
+      alt: 'Coding Quiz Challenge',
+      gitHub: 'https://github.com/TheBluWiz/devQuiz1',
+      deployed: 'https://thebluwiz.github.io/devQuiz1/'
+    },
+    {
+      title: "Giphtionary",
+      image: giphtionaryPic,
+      alt: 'Where words meet giphs',
+      gitHub: 'https://github.com/TheBluWiz/Giphtionary',
+      deployed: 'https://thebluwiz.github.io/Giphtionary/'
+    },
+    {
+      title: "Mail-Me-Seeds!",
+      image: mailMeSeedsPic,
+      alt: 'Welcome to Mail Me Seeds!',
+      gitHub: 'https://github.com/TheBluWiz/mail-me-seeds',
+      deployed: 'https://mail-me-seeds.herokuapp.com/'
+    },
+    {
+      title: "Note Taker",
+      image: noteTaker,
+      alt: 'Take Notes with Express',
+      gitHub: 'https://github.com/TheBluWiz/note-taker',
+      deployed: 'https://note-taker-service.herokuapp.com/'
+    },
+    {
+      title: "Team Profile Generator",
+      image: teamProfileGenerator,
+      alt: 'A grid of employee information',
+      gitHub: 'https://github.com/TheBluWiz/team-profile-generator',
+      deployed: '/'
+    },
+    {
+      title: "Tech Spot Blots",
+      image: techSpotBlot,
+      alt: 'Tech Spot Blots - A Blogging Site',
+      gitHub: 'https://github.com/TheBluWiz/tech-spot-blot',
+      deployed: 'https://tech-spot-blot.herokuapp.com'
+    }
+  ]
+
+
+  return cardArray.map((site) => {
+    return (
+      <div className='card'>
+        <div className='image'>
+          <img src={site.image} alt={site.alt}/>
+        </div>
+        <div className="Buttons">
+
+        </div>
+      </div>
+    )
+  });
 }
