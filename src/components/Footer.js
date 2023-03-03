30f5a57b3b8c5e61a0fef4d9fc252bb765f66158
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-02 17:39:41 -0800

    Footer Links now open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,20 @@ import stackOverflowLogo from '../assets/images/stack-overflow-logo.png'
 export default function Footer() {
   return (
     <div className="bg-emerald-500 fixed bottom-0 grid place-items-center">
-    <div className="grid gap-5 grid-cols-3 place-items-center mx-0 w-4/5">
-      <a href="https://github.com/TheBluWiz">
-        <img src={gitHubLogo} alt="GitHub" />
-      </a>
-      <a href='https://www.linkedin.com/in/jamey-wicklund-19673851/'>
-        <img src={linkedInLogo} alt="LinkedIn" />
-      </a>
-      <a href="https://stackoverflow.com/users/21322792/thebluwiz">
-        <img src={stackOverflowLogo} alt="Stack Overflow" />
-      </a>
-    </div>
+      <div className="grid gap-5 grid-cols-3 place-items-center mx-0 w-4/5">
+        <a href="https://github.com/TheBluWiz"
+          target="_blank">
+          <img src={gitHubLogo} alt="GitHub" />
+        </a>
+        <a href='https://www.linkedin.com/in/jamey-wicklund-19673851/'
+          target="_blank">
+          <img src={linkedInLogo} alt="LinkedIn" />
+        </a>
+        <a href="https://stackoverflow.com/users/21322792/thebluwiz"
+          target="_blank">
+          <img src={stackOverflowLogo} alt="Stack Overflow" />
+        </a>
+      </div>
     </div>
   )
 }
