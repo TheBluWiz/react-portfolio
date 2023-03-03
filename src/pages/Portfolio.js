a0827c798da8e25e4fc5e218832fad12fd2b5f99
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-03 15:29:31 -0800

    Adds grid to Portfolio Cards

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -56,7 +56,9 @@ export default function Contact() {
   ]
 
   return (
-    <Card cardArray={cardArray}/>
-)
-  
+    <div className='md:grid md:grid-cols-2 lg:grid-cols-3'>
+      <Card cardArray={cardArray} />
+    </div>
+  )
+
 }
