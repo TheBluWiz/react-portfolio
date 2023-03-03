4fb14e07faf4d1ba960a71a42ee5d17bedbfeba7
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-02 17:38:01 -0800

    Adds footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from "./components/Header.js";
 import Body from './components/Body.js';
+import Footer from './components/Footer.js'
 
 function App() {
   const [currentPage, setCurrentPage] = useState('AboutMe');
@@ -10,6 +11,7 @@ function App() {
     <div className='bg-slate-200 min-h-screen'>
     <Header currentPage={currentPage} setCurrentPage={setCurrentPage}/>
     <Body currentPage={currentPage} />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import gitHubLogo from '../assets/images/github-long-logo.png';
+import linkedInLogo from '../assets/images/linkedin-logo.png';
+import stackOverflowLogo from '../assets/images/stack-overflow-logo.png'
+
+export default function Footer() {
+  return (
+    <div className="bg-emerald-500 fixed bottom-0 grid place-items-center">
+    <div className="grid gap-5 grid-cols-3 place-items-center mx-0 w-4/5">
+      <a href="https://github.com/TheBluWiz">
+        <img src={gitHubLogo} alt="GitHub" />
+      </a>
+      <a href='https://www.linkedin.com/in/jamey-wicklund-19673851/'>
+        <img src={linkedInLogo} alt="LinkedIn" />
+      </a>
+      <a href="https://stackoverflow.com/users/21322792/thebluwiz">
+        <img src={stackOverflowLogo} alt="Stack Overflow" />
+      </a>
+    </div>
+    </div>
+  )
+}
